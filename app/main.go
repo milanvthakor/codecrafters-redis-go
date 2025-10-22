2b7d1e82058e02562babebb6a71cb306b9e1cdd4
Milan Thakor <milanv.thakor@gmail.com>
2025-10-22 16:20:38 +0530

    Make separate method for each command.

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -9,6 +10,58 @@ import (
 	"strings"
 	"time"
 )
+
+var errInvalidCmd = errors.New("invalid command")
+
+func handleEchoCmd(cmd []*RespVal) (string, error) {
+	if len(cmd) < 2 {
+		return "", errInvalidCmd
+	}
+	return ToSimpleStr(cmd[1].BulkStrs()), nil
+}
+
+func handleSetCmd(cmd []*RespVal) (string, error) {
+	if len(cmd) < 3 {
+		return "", errInvalidCmd
+	}
+
+	var exp time.Duration
+	if len(cmd) > 3 { // Optional exp arg is present
+		if len(cmd) != 5 {
+			return "", errInvalidCmd
+		}
+
+		flag := strings.ToUpper(cmd[3].BulkStrs())
+		dur, err := strconv.ParseInt(cmd[4].BulkStrs(), 10, 64)
+		if err != nil {
+			return "", fmt.Errorf("invalid expiry value")
+		}
+
+		switch flag {
+		case "EX":
+			exp = time.Duration(dur * int64(time.Second))
+		case "PX":
+			exp = time.Duration(dur * int64(time.Millisecond))
+		default:
+			return "", fmt.Errorf("invalid expiry flag")
+		}
+	}
+
+	memCache.Set(cmd[1].BulkStrs(), cmd[2].BulkStrs(), exp)
+	return ToSimpleStr("OK"), nil
+}
+
+func handleGetCmd(cmd []*RespVal) (string, error) {
+	if len(cmd) < 2 {
+		return "", errInvalidCmd
+	}
+
+	if val, ok := memCache.Get(cmd[1].BulkStrs()); !ok {
+		return ToNulls(), nil
+	} else {
+		return ToBulkStr(val), nil
+	}
+}
 
 // handleConnection handles the single client connection
 func handleConnection(conn net.Conn) {
@@ -38,57 +91,19 @@ func handleConnection(conn net.Conn) {
 			respStr = ToSimpleStr("PONG")
 
 		case "ECHO":
-			if len(cmd) < 2 {
-				fmt.Println("invalid command")
-				return
-			}
-
-			respStr = ToSimpleStr(cmd[1].BulkStrs())
+			respStr, err = handleEchoCmd(cmd)
 
 		case "SET":
-			if len(cmd) < 3 {
-				fmt.Println("invalid command")
-				return
-			}
-
-			var exp time.Duration
-			if len(cmd) > 3 { // Optional exp arg is present
-				if len(cmd) != 5 {
-					fmt.Println("invalid command")
-					return
-				}
-
-				flag := strings.ToUpper(cmd[3].BulkStrs())
-				dur, err := strconv.ParseInt(cmd[4].BulkStrs(), 10, 64)
-				if err != nil {
-					fmt.Println("invalid expiry value")
-				}
-
-				switch flag {
-				case "EX":
-					exp = time.Duration(dur * int64(time.Second))
-				case "PX":
-					exp = time.Duration(dur * int64(time.Millisecond))
-				default:
-					fmt.Println("invalid expiry flag")
-					return
-				}
-			}
-
-			memCache.Set(cmd[1].BulkStrs(), cmd[2].BulkStrs(), exp)
-			respStr = ToSimpleStr("OK")
+			respStr, err = handleSetCmd(cmd)
 
 		case "GET":
-			if len(cmd) < 2 {
-				fmt.Println("invalid command")
-				return
-			}
+			respStr, err = handleGetCmd(cmd)
+		}
 
-			if val, ok := memCache.Get(cmd[1].BulkStrs()); !ok {
-				respStr = ToNulls()
-			} else {
-				respStr = ToBulkStr(val)
-			}
+		// Check the error from the command action, if any
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 
 		// Return the response
